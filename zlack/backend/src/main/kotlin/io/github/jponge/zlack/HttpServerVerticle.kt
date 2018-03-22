5df0c5f24e4b8dce8e8c6c1ff9419717861248db
Julien Ponge <julien.ponge@gmail.com>
2018-03-22 22:14:17 +0100

    Fix SockJS not using WebSockets

--- zlack/backend/src/main/kotlin/io/github/jponge/zlack/HttpServerVerticle.kt
+++ zlack/backend/src/main/kotlin/io/github/jponge/zlack/HttpServerVerticle.kt
@@ -7,12 +7,16 @@ import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.ext.bridge.PermittedOptions
 import io.vertx.kotlin.ext.web.handler.sockjs.BridgeOptions
+import io.vertx.kotlin.ext.web.handler.sockjs.SockJSHandlerOptions
 import io.vertx.reactivex.core.AbstractVerticle
 import io.vertx.reactivex.ext.web.Router
 import io.vertx.reactivex.ext.web.RoutingContext
 import io.vertx.reactivex.ext.web.handler.BodyHandler
+import io.vertx.reactivex.ext.web.handler.CookieHandler
+import io.vertx.reactivex.ext.web.handler.SessionHandler
 import io.vertx.reactivex.ext.web.handler.StaticHandler
 import io.vertx.reactivex.ext.web.handler.sockjs.SockJSHandler
+import io.vertx.reactivex.ext.web.sstore.LocalSessionStore
 import org.slf4j.LoggerFactory
 
 class HttpServerVerticle : AbstractVerticle() {
@@ -24,11 +28,11 @@ class HttpServerVerticle : AbstractVerticle() {
     logger.info("Trying to start a HTTP server on port ${port}")
 
     val router = Router.router(vertx)
-    router.route("/api/*").handler(BodyHandler.create())
+    router.route().handler(BodyHandler.create())
     router.get("/api/messages").handler(this::getAllMessages)
     router.post("/api/messages").handler(this::postNewMessage)
 
-    val sockJSHandler = SockJSHandler.create(vertx)
+    val sockJSHandler = SockJSHandler.create(vertx, SockJSHandlerOptions(insertJSESSIONID = false))
     val options = BridgeOptions(
       inboundPermitteds = listOf(PermittedOptions(addressRegex = ".*")),  // ⚠
       outboundPermitteds = listOf(PermittedOptions(addressRegex = ".*"))) // ⚠
