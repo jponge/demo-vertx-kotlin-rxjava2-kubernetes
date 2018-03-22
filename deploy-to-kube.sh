836800cd25bef688744cab0540688ba8d0bbd389
Julien Ponge <julien.ponge@gmail.com>
2018-03-22 14:53:26 +0100

    Upgrade to app/v1 and perform scripted deployments

---
+++ deploy-to-kube.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+eval $(minikube docker-env)
+
+Services=('temperature-service' 'temperature-gateway' 'zlack' 'temperature-to-zlack-service')
+
+for service in "${Services[@]}"; do
+  cd $service
+  docker build -t $service:latest .
+  cd ..
+done
+
+K8sResources=('mongo' 'zlack' 'temperature-service-1' 'temperature-service-2' 'temperature-service-3' 'temperature-gateway')
+
+for yaml in "${K8sResources[@]}"; do
+  kubectl apply -f kubernetes/${yaml}.yaml
+done
